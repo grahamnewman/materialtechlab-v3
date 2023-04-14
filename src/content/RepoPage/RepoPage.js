bc7434daba604fdfaffa9da92846604d5e040de3
Graham Newman MRes (RCA) <graham.newman@network.rca.ac.uk>
2023-04-14 11:48:01 +0700

    sixth commit

--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import { Link, Grid, Column } from '@carbon/react';
+import React, { useEffect, useState } from 'react';
+import { Octokit } from '@octokit/core';
 import RepoTable from './RepoTable';
-import { Grid, Column } from '@carbon/react';
+const octokitClient = new Octokit({});
+
 
 const RepoPage = () => {
+  //this is where we got to in the tutorial Then, inside the RepoPage component:
+  // const [loading, setLoading] = useState(true);
+  // const [error, setError] = useState();
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    async function getCarbonRepos() {
+      const res = await octokitClient.request('GET /orgs/{org}/repos', {
+        org: 'carbon-design-system',
+        per_page: 75,
+        sort: 'updated',
+        direction: 'desc',
+      });
+
+      setRows(getRowItems(res.data));
+    }
+
+    getCarbonRepos();
+  }, []);
+
   return (
     <Grid className="repo-page">
       <Column lg={16} className="repo-page__r1">
@@ -11,6 +34,32 @@ const RepoPage = () => {
     </Grid>
   );
 };
+
+const getRowItems = (rows) =>
+  rows.map((row) => ({
+    ...row,
+    key: row.id,
+    stars: row.stargazers_count,
+    issueCount: row.open_issues_count,
+    createdAt: new Date(row.created_at).toLocaleDateString(),
+    updatedAt: new Date(row.updated_at).toLocaleDateString(),
+    links: <LinkList url={row.html_url} homepageUrl={row.homepage} />,
+  }));
+
+const LinkList = ({ url, homepageUrl }) => (
+  <ul style={{ display: 'flex' }}>
+    <li>
+      <Link href={url}>GitHub</Link>
+    </li>
+    {homepageUrl && (
+      <li>
+        <span>&nbsp;|&nbsp;</span>
+        <Link href={homepageUrl}>Homepage</Link>
+      </li>
+    )}
+  </ul>
+);
+
 
 const headers = [
   {
@@ -39,35 +88,7 @@ const headers = [
   },
 ];
 
-const rows = [
-  {
-    id: '1',
-    name: 'Repo 1',
-    createdAt: 'Date',
-    updatedAt: 'Date',
-    issueCount: '123',
-    stars: '456',
-    links: 'Links',
-  },
-  {
-    id: '2',
-    name: 'Repo 2',
-    createdAt: 'Date',
-    updatedAt: 'Date',
-    issueCount: '123',
-    stars: '456',
-    links: 'Links',
-  },
-  {
-    id: '3',
-    name: 'Repo 3',
-    createdAt: 'Date',
-    updatedAt: 'Date',
-    issueCount: '123',
-    stars: '456',
-    links: 'Links',
-  },
-];
+
 
 
 export default RepoPage;
